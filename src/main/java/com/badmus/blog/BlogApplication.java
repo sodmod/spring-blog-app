807ea142b69c09ccb141b32ddf891ec8c019d27b
sodmod <horluwatosin1999@gmail.com>
2023-11-17 22:14:36 +0100

    updated the property file

--- src/main/java/com/badmus/blog/BlogApplication.java
+++ src/main/java/com/badmus/blog/BlogApplication.java
@@ -1,11 +1,9 @@
 package com.badmus.blog;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class BlogApplication {
 
 	public static void main(String[] args) {
